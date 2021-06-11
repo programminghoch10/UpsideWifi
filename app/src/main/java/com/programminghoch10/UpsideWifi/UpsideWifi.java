7c8af81db7cdce05f048a1e33f06358baf87fba5
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2021-06-12 00:30:00 +0200

    added continuous rotation to wifi symbol

--- app/src/main/java/com/programminghoch10/UpsideWifi/UpsideWifi.java
+++ app/src/main/java/com/programminghoch10/UpsideWifi/UpsideWifi.java
@@ -1,26 +1,39 @@
 package com.programminghoch10.UpsideWifi;
 
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
+
 import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
 import de.robv.android.xposed.callbacks.XC_LayoutInflated;
 
 public class UpsideWifi implements IXposedHookInitPackageResources {
-	ImageView wifiIndicator;
-	
-	@Override
-	public void handleInitPackageResources(InitPackageResourcesParam resparam)
-			throws Throwable {
-		if (!resparam.packageName.equals("com.android.systemui")) {
-			return;
-		}
-		resparam.res.hookLayout("com.android.systemui", "layout", "status_bar_wifi_group", new XC_LayoutInflated() {
-	        @Override
-	        public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
-	            wifiIndicator = (ImageView) liparam.view.findViewById(liparam.res.getIdentifier("wifi_signal", "id", "com.android.systemui"));
-	            wifiIndicator.setRotation(180);
-	        }
-	    });
-	}
+    static final RotateAnimation rotate;
+
+    static {
+        rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setDuration(5000);
+        rotate.setRepeatCount(Animation.INFINITE);
+        rotate.setInterpolator(new LinearInterpolator());
+    }
+
+    ImageView wifiIndicator;
+
+    @Override
+    public void handleInitPackageResources(InitPackageResourcesParam resparam)
+            throws Throwable {
+        if (!resparam.packageName.equals("com.android.systemui")) {
+            return;
+        }
+        resparam.res.hookLayout("com.android.systemui", "layout", "status_bar_wifi_group", new XC_LayoutInflated() {
+            @Override
+            public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
+                wifiIndicator = (ImageView) liparam.view.findViewById(liparam.res.getIdentifier("wifi_signal", "id", "com.android.systemui"));
+                wifiIndicator.startAnimation(rotate);
+            }
+        });
+    }
 
 }
