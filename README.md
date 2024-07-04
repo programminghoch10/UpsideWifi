15be49fb4722e016ffbc5b76b4801c26f78f8736
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2024-07-04 12:39:09 +0200

    update module for UPSIDE_DOWN_CAKE
    status_bar_wifi_group was renamed to new_status_bar_wifi_group
    
    Fixes #2

--- README.md
+++ README.md
@@ -2,5 +2,3 @@
 XPosed Module to turn the WiFi icon upside down.
 
 This is an modified version of the legendary module [UpsideWifi](https://github.com/rye761/upsidewifi) by [rye761](https://github.com/rye761).
-
-This module needs the resource hooks enabled.
--- app/src/main/java/com/programminghoch10/UpsideWifi/UpsideWifi.java
+++ app/src/main/java/com/programminghoch10/UpsideWifi/UpsideWifi.java
@@ -1,5 +1,6 @@
 package com.programminghoch10.UpsideWifi;
 
+import android.annotation.SuppressLint;
 import android.os.Build;
 import android.widget.ImageView;
 
@@ -9,14 +10,17 @@ import de.robv.android.xposed.callbacks.XC_LayoutInflated;
 
 public class UpsideWifi implements IXposedHookInitPackageResources {
     ImageView wifiIndicator;
-
+    
     @Override
     public void handleInitPackageResources(InitPackageResourcesParam resparam) throws Throwable {
         if (!resparam.packageName.equals("com.android.systemui")) {
             return;
         }
-        String layoutName = android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.P ? "status_bar_wifi_group" : "signal_cluster_view";
+        String layoutName =
+                Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE ? "new_status_bar_wifi_group" :
+                        (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P ? "status_bar_wifi_group" : "signal_cluster_view");
         resparam.res.hookLayout("com.android.systemui", "layout", layoutName, new XC_LayoutInflated() {
+            @SuppressLint("DiscouragedApi")
             @Override
             public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
                 wifiIndicator = liparam.view.findViewById(liparam.res.getIdentifier("wifi_signal", "id", "com.android.systemui"));
